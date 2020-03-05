d1f92f92bd2d6d9c3be18f416dd0df660c0c8bd4
GlevDev <garth@glev.fr>
2020-03-05 23:21:18 +0100

    config socket in App.vue

---
+++ client/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <h1>Chatroom</h1>
+      <p class="username">Username: {{ username }}</p>
+      <p class="online">Online: {{ users.length }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      username: "",
+      socket: io("http://localhost:3000"),
+      messages: [],
+      users: []
+    }
+  },
+  methods: {
+    joinServer() {
+      this.socket.on('loggedIn', data => {
+        this.messages = data.messages;
+        this.users = data.users;
+        this.socket.emit('newuser', this.username);
+      });
+
+      this.listen();
+    },
+    listen() {
+      this.socket.on('userOnline', user => {
+        this.users.push(user);
+      });
+      this.socket.on('userLeft', user => {
+        this.users.splice(this.users.indexOf(user), 1)
+      });
+      this.socket.on('msg', message => {
+        this.messages.push(message);
+      })
+    }
+  },
+  mounted: function() {
+    this.username = prompt("What is your username ?", "Anonymous");
+    if(!this.username) {
+      this.username = "Anonymous";
+    }
+    this.joinServer();
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  font-family: 'Avenir', Helvetica, Arial, Helvetica, sans-serif;
+  color: #2C3E50;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+}
+</style>
